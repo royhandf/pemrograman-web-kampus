15f76f2c5fa6135d29d04a95b8d7755a78073348
M. Royhan Daffa <70930874+royhandf@users.noreply.github.com>
2022-05-11 20:37:33 +0700

    tambah modul 12

---
+++ Modul12/modify_cookie.php
@@ -0,0 +1,23 @@
+<?php
+    $cookie_name = "user";
+    $cookie_value = "Alex Porter";
+    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); 
+    // 86400 = 1 day
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        if(!isset($_COOKIE[$cookie_name])) {
+            echo "Cookie named '" . $cookie_name . "' is not set!";
+        } else {
+            echo "Cookie '" . $cookie_name . "' is set!<br>";
+            echo "Value is: " . $_COOKIE[$cookie_name];
+        }
+    ?>
+</body>
+</html>
